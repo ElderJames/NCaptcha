4ac7d904cde4e17ac3d26e6e8037649cd5abe3db
ElderJames <shunjiey@hotmail.com>
2019-09-05 11:36:41 +0800

    add a sample for session-based image captcha

---
+++ samples/NCaptcha.Samples.SessionImages/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NCaptcha.Samples.SessionImages.Data;
+
+[assembly: HostingStartup(typeof(NCaptcha.Samples.SessionImages.Areas.Identity.IdentityHostingStartup))]
+namespace NCaptcha.Samples.SessionImages.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
